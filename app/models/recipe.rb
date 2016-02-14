e675fe42fc942e06f8cb4905066421c480ec46be
David VanSlamBrouck <spire2985@gmail.com>
2016-02-14 03:52:09 -0600

    Add Chapter 6 steps

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ActiveRecord::Base
   validates_presence_of :title, :ingredients, :instructions
+  validates :title, uniqueness: true
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -27,4 +27,10 @@ class RecipeTest < ActiveSupport::TestCase
     assert !recipe.save # save should fail because there are errors.
     assert recipe.errors[:instructions].include?("can't be blank")
   end
+  
+  test "should only have one recipe with the same name" do
+    @recipe = Recipe.new(:title => "Toast")
+    @recipe.valid?
+    assert @recipe.errors[:title].include?("must be unique")
+  end
 end
